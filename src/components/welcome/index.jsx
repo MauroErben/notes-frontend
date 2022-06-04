48726c94fd93349b170c11588720847a1a7f0be9
Mauro Erben <mauroerben100@hotmail.com>
2022-06-04 07:07:18 -0300

    added welcome screen and footer

--- src/components/welcome/index.jsx
+++ src/components/welcome/index.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
-import { Box, Text } from '@chakra-ui/react'
+import { VStack, Heading, Text, Button, chakra } from '@chakra-ui/react'
 
 function Welcome () {
   return (
-    <Box>
-      <Text>Welcome screen</Text>
-    </Box>
+    <VStack
+      height='90vh'
+      spacing={8}
+      alignItems='center'
+      justifyContent='center'
+      padding={[2, 4, 6, 8]}
+      textAlign='center'
+      fontSize={['xl', '2xl', '3xl']}
+    >
+      <Heading as='h1' color='blue.500'>ONLINE NOTES</Heading>
+      <Heading><chakra.span color='blue.500'>¡Hola!</chakra.span> Bienvenido/a al creador de notas online, para comenzar a crear notas, debes iniciar sesión en tu cuenta</Heading>
+      <Text>Al crear una cuenta, podrás crear y administrar todas las notas que necesites de una manera fácil y rápida.</Text>
+      <Button
+        colorScheme='blue'
+      >
+        ¡Comencemos!
+      </Button>
+    </VStack>
   )
 }
 export default Welcome
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,17 +4,21 @@ import Welcome from '../components/welcome'
 import Home from '../components/home'
 import RegisterForm from '../components/auth/registerForm'
 import LoginForm from '../components/auth/loginForm'
+import Footer from '../components/footer'
 
 function Routes () {
   return (
-    <Router>
-      <Switch>
-        <Route path='/' element={<Home />} />
-        <Route path='/welcome' element={<Welcome />} />
-        <Route path='/auth/register' element={<RegisterForm />} />
-        <Route path='/auth/login' element={<LoginForm />} />
-      </Switch>
-    </Router>
+    <>
+      <Router>
+        <Switch>
+          <Route path='/' element={<Home />} />
+          <Route path='/welcome' element={<Welcome />} />
+          <Route path='/auth/register' element={<RegisterForm />} />
+          <Route path='/auth/login' element={<LoginForm />} />
+        </Switch>
+      </Router>
+      <Footer />
+    </>
   )
 }
 export default Routes
