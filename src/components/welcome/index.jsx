96f8d3755f21a0e227348315bac177ac79b31ab8
Mauro Erben <mauroerben100@hotmail.com>
2022-06-05 10:17:26 -0300

    changes in welcome page

--- src/components/welcome/index.jsx
+++ src/components/welcome/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { VStack, Heading, Text, Button, chakra } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom'
 
 function Welcome () {
+  const navigate = useNavigate()
+
   return (
     <VStack
       height='90vh'
@@ -12,11 +15,22 @@ function Welcome () {
       textAlign='center'
       fontSize={['xl', '2xl', '3xl']}
     >
-      <Heading as='h1' color='blue.500'>ONLINE NOTES</Heading>
-      <Heading><chakra.span color='blue.500'>¡Hola!</chakra.span> Bienvenido/a al creador de notas online, para comenzar a crear notas, debes iniciar sesión en tu cuenta</Heading>
-      <Text>Al crear una cuenta, podrás crear y administrar todas las notas que necesites de una manera fácil y rápida.</Text>
+      <Heading as='h1' color='blue.500'>
+        ONLINE NOTES
+      </Heading>
+
+      <Heading>
+        <chakra.span color='blue.500'>¡Hola! </chakra.span>
+        Bienvenido/a al creador de notas online, para comenzar a crear notas, debes iniciar sesión en tu cuenta
+      </Heading>
+
+      <Text>
+        Al crear una cuenta, podrás crear y administrar todas las notas que necesites de una manera fácil y rápida.
+      </Text>
+
       <Button
         colorScheme='blue'
+        onClick={() => navigate('/auth/login')}
       >
         ¡Comencemos!
       </Button>
