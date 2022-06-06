4ce01efdf964b3070041367a05c970455f42b23f
Mauro Erben <mauroerben100@hotmail.com>
2022-06-06 12:29:09 -0300

    added notes form

---
+++ src/components/notes/notesForm.jsx
@@ -0,0 +1,115 @@
+import React from 'react'
+import {
+  Stack,
+  VStack,
+  Box,
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Textarea
+} from '@chakra-ui/react'
+import { Formik } from 'formik'
+import * as Yup from 'yup'
+import InputError from '../auth/inputError'
+import { useNavigate } from 'react-router-dom'
+
+const notesSchema = Yup.object().shape({
+  title: Yup.string().required('Title is required')
+})
+
+function NotesForm () {
+  const navigate = useNavigate()
+
+  const handleNewNote = (values, { setSubmitting, resetForm }) => {
+    setTimeout(() => {
+      console.log(values)
+      setSubmitting(false)
+      resetForm()
+    }, 400)
+  }
+
+  return (
+    <Stack
+      height='90vh'
+      justify='center'
+      align='center'
+    >
+      <Formik
+        enableReinitialize
+        initialValues={{ title: '', note: '' }}
+        validationSchema={notesSchema}
+        onSubmit={handleNewNote}
+      >
+        {({ handleSubmit, handleChange, handleBlur, touched, errors, values, isSubmitting }) => (
+          <VStack
+            width={[300, 400, 500]}
+            boxShadow='lg'
+            borderRadius='md'
+          >
+            <Box
+              width='full'
+              padding={8}
+              backgroundColor='blue.500'
+              clipPath='polygon(0 0, 100% 0, 100% 100%, 0 80%)'
+              color='white'
+              textAlign='center'
+            >
+              <Heading as='h1' size='lg'>ONLINE NOTES</Heading>
+              <Heading as='h3' size='sm'>Nueva nota</Heading>
+            </Box>
+
+            <VStack
+              width='full'
+              padding={[2, 4, 6, 8]}
+              spacing={4}
+            >
+              <FormControl>
+                <FormLabel>Titulo</FormLabel>
+                <Input
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.title}
+                  name='title'
+                  type='text'
+                  placeholder='Titulo de la nota'
+                />
+                {errors.title && touched.title && <InputError>{errors.title}</InputError>}
+              </FormControl>
+
+              <FormControl>
+                <FormLabel>Nota</FormLabel>
+                <Textarea
+                  name='note'
+                  value={values.note}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  placeholder='Ingresa una nota'
+                />
+                {errors.note && touched.note && <InputError>{errors.note}</InputError>}
+              </FormControl>
+
+              {/* FOOTER */}
+              <Button
+                type='submit'
+                colorScheme='blue'
+                disabled={isSubmitting}
+                onClick={handleSubmit}
+              >
+                Crear nota
+              </Button>
+              <Button
+                colorScheme='red'
+                onClick={() => navigate('/')}
+              >
+                Cancelar
+              </Button>
+            </VStack>
+          </VStack>
+        )}
+      </Formik>
+    </Stack>
+  )
+}
+export default NotesForm
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,7 +4,8 @@ import Welcome from '../components/welcome'
 import Home from '../components/home'
 import RegisterForm from '../components/auth/registerForm'
 import LoginForm from '../components/auth/loginForm'
-import Footer from '../components/footer'
+// import Footer from '../components/footer'
+import NotesForm from '../components/notes/notesForm'
 
 function Routes () {
   return (
@@ -15,9 +16,10 @@ function Routes () {
           <Route path='/welcome' element={<Welcome />} />
           <Route path='/auth/register' element={<RegisterForm />} />
           <Route path='/auth/login' element={<LoginForm />} />
+          <Route path='/create' element={<NotesForm />} />
         </Switch>
       </Router>
-      <Footer />
+      {/* <Footer /> */}
     </>
   )
 }
