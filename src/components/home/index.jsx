4b29413763d8167408d6131513838d4c55a98e35
Mauro Erben <mauroerben100@hotmail.com>
2022-06-06 10:03:44 -0300

    protection in home route

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import { Box, Text } from '@chakra-ui/react'
+import { Navigate } from 'react-router-dom'
 
 function Home () {
+  const token = localStorage.getItem('online-notes')
+
+  if (!token) {
+    return (<Navigate to='/auth/login' replace />)
+  }
+
   return (
     <Box>
       <Text>Home</Text>
