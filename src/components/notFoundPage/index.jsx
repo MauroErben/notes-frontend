053c874510b236dc1938ca6d6aa8485162b25d37
Mauro Erben <mauroerben100@hotmail.com>
2022-06-08 15:12:06 -0300

    footer removed and added not found page

---
+++ src/components/notFoundPage/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function NotFound () {
+  return (
+    <div>
+      <h1>Página no encontrada</h1>
+    </div>
+  )
+}
+export default NotFound
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,14 +4,15 @@ import Welcome from '../components/welcome'
 import Home from '../components/home'
 import RegisterForm from '../components/auth/registerForm'
 import LoginForm from '../components/auth/loginForm'
-// import Footer from '../components/footer'
 import NotesForm from '../components/notes/notesForm'
+import NotFound from '../components/notFoundPage'
 
 function Routes () {
   return (
     <>
       <Router>
         <Switch>
+          <Route path='*' element={<NotFound />} />
           <Route path='/' element={<Home />} />
           <Route path='/welcome' element={<Welcome />} />
           <Route path='/auth/register' element={<RegisterForm />} />
@@ -19,7 +20,6 @@ function Routes () {
           <Route path='/create' element={<NotesForm />} />
         </Switch>
       </Router>
-      {/* <Footer /> */}
     </>
   )
 }
